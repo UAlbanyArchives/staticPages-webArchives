e3a303b6a3b4af1d924590cdc631dd0610779833
bhouston1982 <houstobn@uwm.edu>
2017-02-22 09:55:57 -0600

    Ch-ch-changes
    I'm going to need to revert this

--- basicSample.py
+++ basicSample.py
@@ -1,25 +1,42 @@
 import requests
 
-requestURL = "http://wayback.archive-it.org/3308/timemap/cdx?url=http://www.albany.edu/history/course-descriptions.shtml"
+def makeDate(stamp):
+	calendar = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
+	year = stamp[:4]
+	month = stamp[4:6]
+	day = stamp[-2:]
+	normal = year + "-" + month + "-" + day
+	if day.startswith("0"):
+		day = day[-1:]
+	dacs = year + " " + calendar[month] + " " + day
+	return dacs, normal
 
-response = requests.get("http://wayback.archive-it.org/3308/timemap/cdx?url=http://www.albany.edu/history/course-descriptions.shtml")
+requestURL = "http://wayback.archive-it.org/3368/timemap/cdx?url=http://www.uwm.edu/arts/"
+
+response = requests.get(requestURL)
 responseText  = response.text
 
 #variable to count number of captures:
 aiCount = 0
-#if lenght of HTTP response is greater than 5, aribitrary value to check for any captures
-if len(responseText) < 5:
-	print "ERROR: no captures found for "
-	
-	responseLines = responseText.split("\n")
-	firstPage = responseLines[0]
-	for textLine in responseLines:
-		aiCount = aiCount + 1
-		if len(textLine) > 5:
-			lastPage = textLine
+responseLines = responseText.split("\n")
+firstPage = responseLines[0]
+for textLine in responseLines:
+	aiCount = aiCount + 1
+	if len(textLine) > 5:
+		lastPage = textLine
 	#get date range of captures
-	firstDate = firstPage.split(" ")[1][:8]
-	lastDate = lastPage.split(" ")[1][:8]
+firstDate = firstPage.split(" ")[1][:8]
+lastDate = lastPage.split(" ")[1][:8]
 	
 print str(aiCount) + " captures"
 print "from " + firstDate + " to " + lastDate
+
+dateRange = [firstDate, lastDate]
+seriesMax = max(dateRange)
+seriesMin = min(dateRange)
+seriesMaxDacs, seriesMaxNormal = makeDate(seriesMax)
+seriesMinDacs, seriesMinNormal = makeDate(seriesMin)
+seriesDacs = seriesMinDacs + "-" + seriesMaxDacs
+seriesNormal = seriesMinNormal + "/" + seriesMaxNormal
+print "DACS date range: " + seriesDacs
+print "Normal (ISO) date range: " + seriesNormal
